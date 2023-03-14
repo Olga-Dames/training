8daa093af3903ae9ca706c5ee18d6bcf06001313
Olga-Dames <113420629+Olga-Dames@users.noreply.github.com>
2023-03-14 14:31:35 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,28 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+// import Notiflix from 'notiflix';
+console.log('hello');
+
+// const refs = {
+//   form: document.querySelector('.form'),
+//   btn: document.querySelector('button')
+// }
+
+// refs.form.addEventListener('submit', onSubmit)
+// console.log('hello');
+// function onSubmit(e) {
+//   e.preventDefault();
+//   console.log('hello');
+// }
+// function createPromise(position, delay) {
+
+//   // const shouldResolve = Math.random() > 0.3;
+//   // const promise = new Promise((resolve,reject) => {
+
+//   // })
+
+  
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
